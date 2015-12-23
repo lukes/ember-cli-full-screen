18458304cf970eb729949145213b7e6991d5fa92
Luke Duncalfe <lduncalfe@eml.cc>
2015-12-23 21:29:33 +1300

    Updating gh-pages

--- tests/dummy/app/components/full-screen-demo.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-import FullScreenMixin from 'ember-cli-full-screen/mixins/full-screen';
-
-export default Ember.Component.extend(FullScreenMixin, {
-  elementId: 'full-screen-demo'
-});
-

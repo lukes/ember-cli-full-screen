cab1c58f924956fc00dc04e21b22812db727b49f
Luke Duncalfe <lduncalfe@eml.cc>
2015-12-23 16:26:48 +1300

    Adding a demo to dummy app.

---
+++ tests/dummy/app/components/full-screen-demo.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+import FullScreenMixin from 'ember-cli-full-screen/mixins/full-screen';
+
+export default Ember.Component.extend(FullScreenMixin, {
+  elementId: 'full-screen-demo'
+});
+
